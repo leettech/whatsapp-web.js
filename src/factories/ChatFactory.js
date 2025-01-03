305da2cc9b24859fc0c0eb236cb5e2ba27995a48
Lucas Pereira <lucasp28.contato@gmail.com>
2025-01-03 10:49:06 -0300

    fix chat group creation

--- src/factories/ChatFactory.js
+++ src/factories/ChatFactory.js
@@ -5,7 +5,9 @@ const GroupChat = require('../structures/GroupChat');
 
 class ChatFactory {
     static create(client, data) {
-        if(data.isGroup) {
+        // data.isGroup não existe mais no objeto do chat.
+        // Verificando se é um grupo pelo atributor groupMetadata
+        if (data.groupMetadata) {
             return new GroupChat(client, data);
         }
 
